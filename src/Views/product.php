40c145d7e5c206933b1c1cf9d4f5e7ddffa3a340
razscer0@gmail.com <razscer0@gmail.com>
2023-12-04 14:47:07 +0700

    feat: cashier/implementation

--- src/Views/product.php
+++ src/Views/product.php
@@ -155,6 +155,18 @@ if (!isset($TPL)) {
 <script>
     const productContainer = document.getElementById('default-tab-content');
 
+    if (productContainer !== undefined) {
+        const loadingEl = `<div class="w-full flex justify-center h-[15rem] align-middle" id="container-spinner"><div role="status" id="loading-el" class="m-auto">
+    <svg aria-hidden="true" class="inline w-10 h-10text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+        <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+    </svg>
+    <span class="sr-only">Loading...</span>
+</div></div>`
+
+        productContainer.innerHTML += loadingEl
+    }
+
     if (productContainer) {
         fetch('/products', {
                 method: 'GET'
@@ -162,8 +174,14 @@ if (!isset($TPL)) {
             .then(response => response.json())
             .then(result => {
                 let elements = '';
+                // let other = `<div class="p-4 rounded-lg" id="allItems" role="tabpanel" aria-labelledby="allItems-tab"> </div>`
+
+                let allItem = ''
+                let foodItems = ''
+                let drinkItems = ''
+
                 result.data.forEach(content => {
-                    elements += `<div class="p-4 rounded-lg" id="allItems" role="tabpanel" aria-labelledby="allItems-tab">
+                    allItem += `
                     <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
                         <div>
                             <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
@@ -177,11 +195,53 @@ if (!isset($TPL)) {
                                     <p class="mb-3 text-xl font-bold">Rp. ${content.price}</p>
                                 </div>
                             </div>
-                        </div>
-                    </div>`
+                        </div>`
+
+                    if (content.category === 'food') {
+                        foodsItems += `<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div>
+                            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                                <a href="#">
+                                    <img class="rounded-t-lg" src="https://www.indomie.com/uploads/product/indomie-mi-goreng-special_detail_094906814.png" alt="" />
+                                </a>
+                                <div class="p-3 bg-blue-300 rounded-b-lg flex flex-col items-center justify-center">
+                                    <a href="#">
+                                        <h5 class="mb-2 text-lg font-normal tracking-tight">${content.name}</h5>
+                                    </a>
+                                    <p class="mb-3 text-xl font-bold">Rp. ${content.price}</p>
+                                </div>
+                            </div>
+                        </div>`
+                    }
+
+                    if (content.category === 'drink') {
+                        drinkItems += `<div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                        <div>
+                            <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+                                <a href="#">
+                                    <img class="rounded-t-lg" src="https://www.indomie.com/uploads/product/indomie-mi-goreng-special_detail_094906814.png" alt="" />
+                                </a>
+                                <div class="p-3 bg-blue-300 rounded-b-lg flex flex-col items-center justify-center">
+                                    <a href="#">
+                                        <h5 class="mb-2 text-lg font-normal tracking-tight">${content.name}</h5>
+                                    </a>
+                                    <p class="mb-3 text-xl font-bold">Rp. ${content.price}</p>
+                                </div>
+                            </div>
+                        </div>`
+                    }
 
                 });
-                console.log(elements)
+
+                let allitem = `<div class="p-4 rounded-lg" id="allItems" role="tabpanel" aria-labelledby="allItems-tab"> ${allItem} </div>`
+                let foods = `<div class="p-4 rounded-lg" id="food" role="tabpanel" aria-labelledby="food-tab"> ${foodItems} </div>`
+                let drinks = `<div class="hidden p-4 rounded-lg" id="drink" role="tabpanel" aria-labelledby="drink-tab"> ${drinkItems} </div>`
+
+                elements += allitem
+                elements += foods
+                elements += drinks;
+
+                document.getElementById('container-spinner').remove()
                 productContainer.innerHTML += elements;
 
             }).catch(error => console.error(error))
